de25356d3129e5e414808acd672f2f5c949e4e4f
Joshua Chamberlain <jospcham@ut.utm.edu>
2022-02-17 20:01:02 -0600

    Added functionality to All classes and interfaces. Added function and file comments

--- Decorator/Program.cs
+++ Decorator/Program.cs
@@ -3,6 +3,7 @@
 namespace Decorator
 {
    
+    //
     class DecoratorDemo
     {
 
@@ -23,23 +24,28 @@ namespace Decorator
             Console.WriteLine("Testing StateDecorator");
             StateD.draw();
         }
+        //Allows a Widget to be created.
         abstract class Decorator : Widget
         {
             private Widget wid;
+            //Constructor for decorator.
             public Decorator(Widget w)
             {
                 wid = w;
             }
+            //The base draw for every widget and subclasses.
             public virtual void draw()
             {
                 wid.draw();
             }
 
         }
+        //Allows the creation of text field and provides proof of it being called with draw.
         class TextField : Widget
         {
             private int width;
             private int height;
+            //Constructor for TextField. Sets width and height.
             public TextField(int w, int h)
             {
                 width = w;
@@ -50,36 +56,45 @@ namespace Decorator
                 Console.WriteLine("I am a TextField with width " + width + " and height " + height);
             }
         }
+        //Allows a border decorator to call the base constructor.
         class BorderDecorator : Decorator
         {
+            //Constructor for BorderDecorator. Uses base constructor.
             public BorderDecorator(Widget w) : base(w)
             {
 
             }
+            //Prints text for testing.
             public override void draw()
             {
                 Console.WriteLine("I am a BorderDecorator holding a: ");
                 base.draw();
             }
         }
+        //Allows a scroll decorator to call the base constructor.
         class ScrollDecorator : Decorator
         {
+            //Constructor for ScrollDecorator. Uses base constructor.
             public ScrollDecorator(Widget w) : base(w)
             {
                 
             }
+            //Prints text for testing
             public override void draw()
             {
                 Console.WriteLine("I am a Scroll Decorator holding a: ");
                 base.draw();
             }
         }
+        //Allows a state decorator to call the base constructor.
         class StateDecorator : Decorator
         {
+            //Constructor for StateDecorater. Uses base constructor.
             public StateDecorator(Widget w) : base(w)
             {
 
             }
+            //Prints text for testing.
             public override void draw()
             {
                 Console.WriteLine("I am a StateDecorator holding a: ");
